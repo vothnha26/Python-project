03f1e3aaa99358a511cc8e9e47226021e9e0a912
vothnha26 <vothanhnha26@gmail.com>
2024-11-19 23:10:02 +0700

    update main.py

--- project/main.py
+++ project/main.py
@@ -1,7 +1,6 @@
 from modules import visualization as vs
 
 if __name__ == "__main__":
-    vs.create_db()  # Tạo cơ sở dữ liệu và bảng nếu chưa tồn tại
     root = vs.tk.Tk()
     app = vs.App(root, "./data/data.csv")
     root.mainloop()
